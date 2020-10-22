1aae92c26cdfa3d748cf1a907f10e64765f96976
Tami Takada <tami@mytalos.com>
2020-10-22 18:24:15 -0400

    method largestInRows success

--- ArrayOps.java
+++ ArrayOps.java
@@ -9,11 +9,27 @@ public class ArrayOps {
   }
 
   public static int largest(int[] arr) {
-      int largest = 0;
+      int largest = arr[0];
       for (int i = 0; i < arr.length; i++) {
         if (arr[i] > largest) largest = arr[i];
       }
       return largest;
+  }
+
+  public static int[] sumRows(int[][] matrix) {
+    int[] sums = new int[matrix.length];
+    for (int i = 0; i < matrix.length; i++) {
+      sums[i] = sum(matrix[i]);
+    }
+    return sums;
+  }
+
+  public static int[] largestInRows(int[][] matrix) {
+    int[] largests = new int[matrix.length];
+    for (int i = 0; i < matrix.length; i++) {
+      largests[i] = largest(matrix[i]);
+    }
+    return largests;
   }
 
 }
--- Tester.java
+++ Tester.java
@@ -4,21 +4,28 @@ import java.util.Random;
 public class Tester {
 
   public static ArrayOps arrayOps = new ArrayOps();
-
   public static int[] testOne = new int[] {
     1, 2, 3, 4
   };
-
   public static int[] testTwo = new int[] {
     -10, 0, 33, 1218, -9, -12345
   };
-
   public static int[] testThree = new int[8];
   public static int[] testFour = new int[0];
+
+  public static int[][] testFive = new int[][] {
+    {0, 1, 2, 3, 4},
+    {5, 6, 7, 8, 9, 10, 11, 12},
+    {-1, -2, -5, 2},
+    {-80}
+  };
+  public static int[][] testSix = new int[4][4];
 
   public static void main(String[] args) {
     System.out.println(testSum());
     System.out.println(testLargest());
+    System.out.println(testSumRows());
+    System.out.println(testLargestInRows());
   }
 
   public static boolean testSum() {
@@ -49,7 +56,7 @@ public class Tester {
     for (int i = 0; i < 100; i++) {
       int[] test = createNewArray();
 
-      int largest = 0;
+      int largest = test[0];
       for (int j = 0; j < test.length; j++) {
         if (test[j] > largest) largest = test[j];
       }
@@ -60,15 +67,73 @@ public class Tester {
     return true;
   }
 
+  public static boolean testSumRows() {
+    int[] fiveResult = arrayOps.sumRows(testFive);
+    if (fiveResult[0] != 10) return false;
+    if (fiveResult[1] != 68) return false;
+    if (fiveResult[2] != -6) return false;
+    if (fiveResult[3] != -80) return false;
+
+    for (int i = 0; i < testSix.length; i++) {
+      if (arrayOps.sumRows(testSix)[i] != 0) return false;
+    }
+
+    for (int i = 0; i < 100; i++) {
+      int[][] test = createNew2DArray();
+      int[] result = arrayOps.sumRows(test);
+
+      for (int j = 0; j < test.length; j++) {
+        if (result[j] != arrayOps.sum(test[j])) return false;
+      }
+    }
+
+    return true;
+  }
+
+  public static boolean testLargestInRows() {
+    int[] fiveResult = arrayOps.largestInRows(testFive);
+    if (fiveResult[0] != 4) return false;
+    if (fiveResult[1] != 12) return false;
+    if (fiveResult[2] != 2) return false;
+    if (fiveResult[3] != -80) return false;
+
+    for (int i = 0; i < testSix.length; i++) {
+      if (arrayOps.largestInRows(testSix)[i] != 0) return false;
+    }
+
+    for (int i = 0; i < 100; i++) {
+      int[][] test = createNew2DArray();
+      int[] result = arrayOps.largestInRows(test);
+
+      for (int j = 0; j < test.length; j++) {
+        if (result[j] != arrayOps.largest(test[j])) return false;
+      }
+    }
+
+    return true;
+  }
+
   public static int[] createNewArray() {
     Random rng = new Random();
-    int[] newArray = new int[Math.abs(rng.nextInt(101))];
+    int[] newArray = new int[Math.abs(rng.nextInt(101)) + 1];
 
     for (int i = 0; i < newArray.length; i++) {
       newArray[i] = rng.nextInt();
     }
 
     return newArray;
+  }
+
+  public static int[][] createNew2DArray() {
+    Random rng = new Random();
+    int rows = Math.abs(rng.nextInt(101)) + 1;
+    int[][] toReturn = new int[rows][];
+
+    for (int i = 0; i < rows; i++) {
+      toReturn[i] = createNewArray();
+    }
+
+    return toReturn;
   }
 
 }
